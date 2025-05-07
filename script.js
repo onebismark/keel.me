73a1331cdde2597161c4573c48e990f280d8bbcc
alksjhdlak <gitlab.swimsuit366@passmail.net>
2025-05-06 21:37:31 -0500

    added sounds to js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let themeToggle = document.querySelector('#theme-toggle');
+let clickSound = new Audio('click.mp3');
+let lightModeSound = new Audio('audio/light_side.wav');
+let darkModeSound = new Audio('audio/dark_side.wav');
+
+clickSound.volume = 0.25;
+lightModeSound.volume = 0.25;
+darkModeSound.volume = 0.25;
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+    clickSound.play();
 };
 
 themeToggle.onclick = () => {
@@ -14,6 +22,12 @@ themeToggle.onclick = () => {
     localStorage.setItem('theme', newTheme);
 
     themeToggle.textContent = newTheme === 'dark' ? 'Light Side' : 'Dark Side';
+
+    if (newTheme === 'dark') {
+        darkModeSound.play();
+    } else {
+        lightModeSound.play();
+    }
 };
 
 window.onload = () => {
